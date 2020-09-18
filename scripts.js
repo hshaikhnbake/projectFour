036bdc1866e568874392b3561bd30c099060fe18
Haris Shaikh <shaikh.m.haris@gmail.com>
2020-09-18 17:54:14 -0400

    second commit

--- scripts.js
+++ scripts.js
@@ -1,39 +1,58 @@
-
-    const cities = ['Beirut', 'Charleston, South Carolina', 'Chicago', 'Florence', 'New Orleans', 'New York City', 'Milan', 'Rome', 'Paris', 'Singapore']
+    const cities = ['Beirut', 'New Orleans', 'Florence', 'Rome', 'Charleston', 'Singapore', 'Paris', 'New York City', 'Bologna', 'Chicago']
 
     $(function () {
 
-    const restaurantApp = {};
+      const restaurantApp = {};
 
-    restaurantApp.apiKey = 'f2af26c3d6f76bd1f3cc5cba0be539fa';
+      restaurantApp.apiKey = 'f2af26c3d6f76bd1f3cc5cba0be539fa';
 
-    function getCityDetails(city) {
-    return $.ajax({
-    url: `https://developers.zomato.com/api/v2.1/locations?query=${city}`,
-        dataType: 'JSON',
-        method: 'GET',
-        headers: {
-    'user-key': restaurantApp.apiKey
-        }
-
-    }).then((res) => {
-        const cityId = res['location_suggestions'][0].entity_id;
-        function getCityRestaurants(cityId, city) {
+      function getCityDetails(city) {
         return $.ajax({
-    url: `https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&q=${city}&count=10&sort=rating&order=desc`,
-            dataType: 'JSON',
-            method: 'GET',
-            headers: {
-    'user-key': restaurantApp.apiKey
-            }
+          url: `https://developers.zomato.com/api/v2.1/locations?query=${city}`,
+          dataType: 'JSON',
+          method: 'GET',
+          headers: {
+          'user-key': restaurantApp.apiKey
+          }
+          
         }).then((res) => {
-    console.log(res);
+          const cityId = res['location_suggestions'][0].entity_id;
+          const cityTitle = res['location_suggestions'][0].title;
+          function getCityRestaurants(cityId, city) {
+            return $.ajax({
+          url: `https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&q=${city}&count=10&sort=rating&order=desc`,
+              dataType: 'JSON',
+              method: 'GET',
+              headers: {
+          'user-key': restaurantApp.apiKey
+              }
+            }).then((res) => {
+              const cityRestaurants = res;
+              console.log(res);
+              const restaurantList = cityRestaurants['restaurants'];
+              restaurantList.forEach((restaurant) => {
+
+          $.each(restaurant, function (key, value) {
+            const restaurantDetails = value;
+            // console.log(restaurantDetails);
+            console.log(restaurantDetails.name);
+
+            $('.cityTitle').text(cityTitle)
+            
+            $('.restuarantList').append(`
+                    <button>
+                      <li>
+                        <h3>${restaurantDetails.name}</h3>
+                        <p>${restaurantDetails.cuisines}</p>
+                      </li>
+                    </button>
+                  `);
+          })
         })
-
-        }
-        getCityRestaurants(cityId, cities[0]);
-    })
-    }
-    getCityDetails(cities[0]);
-
-});
+            })
+          }
+          getCityRestaurants(cityId, cities[0]);
+        })
+      }
+      getCityDetails(cities[0]);
+    });
